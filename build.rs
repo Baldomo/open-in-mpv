e0d4c446dba9ff579b061dd3bbbccc7a8c5c3726
Baldomo <leobaldin.2000@gmail.com>
2019-07-25 16:39:10 +0200

    Added license, split code in lib.rs and main.rs, simplified methods, added build.rs (to package webexts, WIP)

---
+++ build.rs
@@ -0,0 +1,83 @@
+use std::fs::{read, rename, File};
+use std::io::Write;
+use std::process::Command;
+
+use byteorder::{LittleEndian, WriteBytesExt};
+
+use openssl::error::ErrorStack;
+use openssl::hash::MessageDigest;
+use openssl::pkey::{PKey, Private};
+use openssl::sign::Signer;
+
+type Buffer = Vec<u8>;
+
+fn main() {
+    zip_dir("Chrome", "Chrome/ext.zip");
+    package_crx("./Chrome/ext.zip", "cert.pem");
+    
+    zip_dir("Firefox", "Firefox/ext.zip");
+    package_xpi("Firefox/ext.zip");
+}
+
+fn zip_dir(path: &str, out: &str) {
+    Command::new("zip")
+        .arg("-FS")
+        .arg(out)
+        .arg(path)
+        .output()
+        .unwrap();
+}
+
+fn package_xpi(source: &str) {
+    let trimmed: &str = source.trim_end_matches(".zip");
+    rename(source, format!("{}.xpi", trimmed)).unwrap();
+}
+
+fn package_crx(source: &str, pem: &str) {
+    // read input file and store them into buffers
+    let mut source_buff = read(source).unwrap();
+
+    let user_key = load_key_from_file(pem).unwrap();
+
+    let mut public_key_content: Vec<u8> = user_key.public_key_to_der().unwrap();
+
+    let mut signer = Signer::new(MessageDigest::sha1(), &user_key).unwrap();
+
+    signer.update(&source_buff).unwrap();
+
+    let mut signature: Vec<u8> = signer.sign_to_vec().unwrap();
+
+    let mut pub_key_len: Vec<u8> = vec![];
+    pub_key_len
+        .write_u32::<LittleEndian>(public_key_content.len() as u32)
+        .unwrap();
+
+    let mut sig_len: Vec<u8> = vec![];
+    sig_len
+        .write_u32::<LittleEndian>(signature.len() as u32)
+        .unwrap();
+
+    let trimmed_filename: &str = source.trim_end_matches(".zip");
+    let mut package_file: File = File::create(format!("{}.crx", trimmed_filename)).unwrap();
+    let mut package_buffer: Vec<u8> = Vec::new();
+
+    let mut magic_bytes: Buffer = vec![0x43, 0x72, 0x32, 0x34];
+    let mut version: Buffer = vec![0x02, 0x00, 0x00, 0x00];
+
+    package_buffer.append(&mut magic_bytes);
+    package_buffer.append(&mut version);
+    package_buffer.append(&mut pub_key_len);
+    package_buffer.append(&mut sig_len);
+    package_buffer.append(&mut public_key_content);
+    package_buffer.append(&mut signature);
+    package_buffer.append(&mut source_buff);
+
+    package_file.write_all(&package_buffer).unwrap();
+}
+
+fn load_key_from_file(path: &str) -> Result<PKey<Private>, ErrorStack> {
+    // Read the private key from the file
+    let pem_buff = read(path).unwrap();
+
+    PKey::private_key_from_pem(&pem_buff)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,6 @@
-use std::{collections::HashMap, env, process::{Command, exit, Stdio}, vec::Vec};
-use url::{percent_encoding::percent_decode, Url};
-
-#[derive(Debug)]
-struct MpvOption {
-    url: String,
-    fullscreen: bool,
-    pip: bool,
-    enqueue: bool,
-}
-
-fn build_option(
-    url: Option<&String>,
-    fullscreen: Option<&String>,
-    pip: Option<&String>,
-    enqueue: Option<&String>,
-) -> MpvOption {
-    MpvOption {
-        url: match url {
-            None => String::new(),
-            Some(s) => percent_decode(s.as_bytes())
-                .decode_utf8()
-                .unwrap()
-                .as_ref()
-                .to_owned(),
-        },
-        fullscreen: match fullscreen {
-            None => false,
-            Some(s) => match s.to_owned().as_ref() {
-                "1" => true,
-                "0" => false,
-                _ => false,
-            },
-        },
-        pip: match pip {
-            None => false,
-            Some(s) => match s.to_owned().as_ref() {
-                "1" => true,
-                "0" => false,
-                _ => false,
-            },
-        },
-        enqueue: match enqueue {
-            None => false,
-            Some(s) => match s.to_owned().as_ref() {
-                "1" => true,
-                "0" => false,
-                _ => false,
-            },
-        },
-    }
-}
-
-fn build_args(mo: MpvOption) -> Vec<String> {
-    let mut ret: Vec<String> = Vec::new();
-    if mo.fullscreen {
-        ret.push("--fs".to_owned());
-    }
-
-    if mo.pip {
-        ret.push("--ontop".to_owned());
-        ret.push("--no-border".to_owned());
-        ret.push("--autofit=384x216".to_owned());
-        ret.push("--geometry=98%:98%".to_owned());
-    }
-
-    if mo.enqueue {
-        // TODO: figure this out
-    }
-
-    ret.push(mo.url);
-
-    return ret;
-}
+use open_in_mpv::MpvOption;
+use std::{collections::HashMap, env, process::{Command, exit, Stdio}};
+use url::Url;
 
 fn main() {
     // mpv:///open?url=XXXXXXX&full_screen=1&pip=1&enqueue=0
@@ -93,56 +22,16 @@ fn main() {
 
     let query: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
 
-    let mo = build_option(
-        query.get("url"),
-        query.get("full_screen"),
-        query.get("pip"),
-        query.get("enqueue"),
+    let mo = MpvOption::new(
+        query.get("url").unwrap(),
+        query.get("full_screen").unwrap(),
+        query.get("pip").unwrap(),
+        query.get("enqueue").unwrap(),
     );
 
     Command::new("mpv")
-        .args(build_args(mo))
+        .args(mo.build_args())
         .stdout(Stdio::null())
         .spawn()
         .expect("failed to open mpv");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_build_option() {
-        let one: String = String::from("1");
-        let zero: String = String::from("0");
-        let encoded_url: String = String::from("https%3A%2F%2Fst3x_plus.cdnfile.info%2Fuser592%2F5543369133e06eedecc4907bfcd0fd45%2FEP.1.360p.mp4%3Ftoken%3DZDpvjlchVpTP0yb_5AsaEw%26expires%3D1562085204%26title%3D(360P%2520-%2520mp4)%2520Terminator%2B4%253A%2BSalvation%2BHD-720p");
-
-        let url_option: Option<&String> = Some(&encoded_url);
-        let fullscreen_option: Option<&String> = Some(&one);
-        let pip_option: Option<&String> = Some(&zero);
-        let enqueue_option: Option<&String> = Some(&one);
-
-        let expected_url: String = String::from("https://st3x_plus.cdnfile.info/user592/5543369133e06eedecc4907bfcd0fd45/EP.1.360p.mp4?token=ZDpvjlchVpTP0yb_5AsaEw&expires=1562085204&title=(360P%20-%20mp4)%20Terminator+4%3A+Salvation+HD-720p");
-
-        let mo: MpvOption = build_option(url_option, fullscreen_option, pip_option, enqueue_option);
-
-        assert_eq!(mo.url, expected_url);
-        assert_eq!(mo.fullscreen, true);
-        assert_eq!(mo.pip, false);
-        assert_eq!(mo.enqueue, true);
-    }
-
-    #[test]
-    fn test_build_args() {
-        let mo: MpvOption = MpvOption {
-            url: String::from("invalid_url_but_who_cares"),
-            fullscreen: true,
-            pip: false,
-            enqueue: true,
-        };
-
-        let args: Vec<String> = build_args(mo);
-        assert_eq!(args[0], "--fs".to_owned());
-        assert_eq!(args[1], "invalid_url_but_who_cares".to_owned());
-    }
 }
