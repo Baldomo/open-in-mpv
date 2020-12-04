81c10e755cec74677e08c5a090b4b5b980513d1b
Baldomo <leobaldin.2000@gmail.com>
2020-12-04 10:52:58 +0100

    Added custom flags and custom video player to extension settings and generted mpv:// URLs, updated README

--- Chrome/common.js
+++ Chrome/common.js
@@ -13,6 +13,13 @@ class Option {
                 });
                 break;
             case "checkbox":
+                document.getElementsByName(this.name).forEach(el => el.checked = value);
+                break;
+            case "select":
+                document.getElementsByName(this.name).forEach(el => el.value = value);
+                break;
+            case "text":
+                document.getElementsByName(this.name).forEach(el => el.value = value);
                 break;
         }
     }
@@ -22,19 +29,26 @@ class Option {
             case "radio":
                 return document.querySelector(`input[name="${this.name}"]:checked`).value;
             case "checkbox":
-                break;
+                return document.querySelector(`input[name="${this.name}"]`).checked;
+            case "select":
+                return document.querySelector(`select[name="${this.name}"]`).value;
+            case "text":
+                return document.querySelector(`select[name="${this.name}"]`).value;
         }
     }
 }
 
-const options = [
+const _options = [
     new Option("iconAction", "radio", "clickOnly"),
     new Option("iconActionOption", "radio", "direct"),
+    new Option("mpvPlayer", "select", "mpv"),
+    new Option("useCustomFlags", "checkbox", false),
+    new Option("customFlags", "text", "")
 ];
 
 export function getOptions(callback) {
     const getDict = {};
-    options.forEach((item) => {
+    _options.forEach((item) => {
         getDict[item.name] = item.defaultValue;
     })
     chrome.storage.sync.get(getDict, callback);
@@ -42,7 +56,7 @@ export function getOptions(callback) {
 
 export function saveOptions() {
     const saveDict = {};
-    options.forEach((item) => {
+    _options.forEach((item) => {
         saveDict[item.name] = item.getValue();
     })
     chrome.storage.sync.set(saveDict);
@@ -50,7 +64,7 @@ export function saveOptions() {
 
 export function restoreOptions() {
     getOptions((items) => {
-        options.forEach((option) => {
+        _options.forEach((option) => {
             option.setValue(items[option.name]);
         });
     });
@@ -58,7 +72,9 @@ export function restoreOptions() {
 
 export function openInMPV(tabId, url, options = {}) {
     const baseURL = `mpv:///open?`;
+    // Encode video URL
     const params = [`url=${encodeURIComponent(url)}`];
+    // Add playback options
     switch (options.mode) {
         case "fullScreen":
             params.push("full_screen=1"); break;
@@ -67,9 +83,16 @@ export function openInMPV(tabId, url, options = {}) {
         case "enqueue":
             params.push("enqueue=1"); break;
     }
+    // Add new window option
     if (options.newWindow) {
         params.push("new_window=1");
     }
+    // Add alternative player and user-defined custom flags
+    getOptions(items => {
+        params.push(`player=${items["mpvPlayer"]}`)
+        if (items["useCustomFlags"])
+            params.push(`flags=${encodeURIComponent(items["customFlags"])}`);
+    })
     const code = `
         var link = document.createElement('a');
         link.href='${baseURL}${params.join("&")}';
--- README.md
+++ README.md
@@ -13,3 +13,23 @@ sudo make install
 
 ### The `mpv://` protocol
 `open-in-mpv install-protocol` will create a custom `xdg-open` desktop file with a scheme handler for the `mpv://` protocol. This lets `xdg-open` call `open-in-mpv` with an encoded URI, so that it can be parsed and the information can be relayed to `mpv` - this logic follows how `iina` parses and opens custom URIs with the `iina://` protocol on Mac. `install-protocol.sh` has the same functionality.
+
+The table below is a simple documentation of the URL query keys and values used to let the `open-in-mpv` executable what to do:
+
+| Key           | Example value                          | Description |
+|---------------|----------------------------------------|---------------|
+| `url`         | `https%3A%2F%2Fyoutu.be%2FdQw4w9WgXcQ` | The actual file URL to be played, URL-encoded |
+| `full_screen` | `1`                                    | Controls whether the video is played in fullscreen mode |
+| `pip`         | `1`                                    | Simulates a picture-in-picture mode (only works with mpv for now) |
+| `enqueue`     | `1`                                    | Adds a video to the queue (see below) |
+| `new_window`  | `1`                                    | Forcibly starts a video in a new window even if one is already open |
+| `player`      | `celluloid`                            | :warning: WIP, starts any arbitrary video player (only mpv-based ones are recommended, such as [Celluloid](https://celluloid-player.github.io/)) |
+| `flags`       | `--vo%3Dgpu`                           | Custom command options and flags to be passed to the video player |
+
+### Playlist and `enqueue` functionality
+For `enqueue` to work properly with any mpv-based player, the playes has to be able to read commands from a `fifo` name pipe. This can be achieved by adding the following lines to any of the user's startup utilities/files, e.g. `.bashrc`, `.zshrc` or `.profile`.
+
+```sh
+mpvsocket=/tmp/mpvsocket
+[[ ! -p /tmp/mpvsocket ]] && mkfifo /tmp/mpvsocket
+```
